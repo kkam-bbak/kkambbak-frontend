c14141b88546699c08292642cc6249bca7ee82ed
minseo <kimmich3737@gmail.com>
2025-10-28 13:36:52 +0900

    Feat: [Req-0005] 메인 페이지 레이아웃 및 라우팅 설정

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable react-refresh/only-export-components */
 import { createBrowserRouter, Navigate, Outlet } from 'react-router-dom'
 import AppLayout from '../components/layout/AppLayout'
+
 import Home from '../pages/Home'
+import MainPage from '../pages/mainPage/mainPage' 
+
 import { useUser } from '../stores/user'
 
 function Protected() {
@@ -14,7 +17,9 @@ export const router = createBrowserRouter([
     path: '/',
     element: <AppLayout />,
     children: [
+
       { index: true, element: <Home /> },
+      { path: 'mainpage', element: <MainPage /> },
       {
         path: 'app',
         element: <Protected />,
