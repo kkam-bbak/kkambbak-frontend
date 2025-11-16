9e6493d66114477c4d0931572cdc08aca7316001
presentKey <kim.hyeonjae4@gmail.com>
2025-11-17 03:54:53 +0900

    Feat: [UI] 마스코트 컴포넌트 구현

---
+++ src/components/Mascot/Mascot.tsx
@@ -0,0 +1,57 @@
+import basic from '@/assets/Character-Basic.png';
+import shining from '@/assets/Character-Shining.png';
+import cute from '@/assets/Character-Cute.png';
+import smile from '@/assets/Character-Smile.png';
+import thinking from '@/assets/Character-Thinking.png';
+import gloomy from '@/assets/Character-Gloomy.png';
+import wrong from '@/assets/Character-Wrong.png';
+import jump from '@/assets/Character-Jump.png';
+import styles from './Mascot.module.css';
+
+type ImageType =
+  | 'basic'
+  | 'shining'
+  | 'cute'
+  | 'smile'
+  | 'thinking'
+  | 'gloomy'
+  | 'wrong'
+  | 'jump';
+
+type MascotProps = {
+  image: ImageType;
+  text: string;
+};
+
+const IMAGE_MAP: Record<ImageType, string> = {
+  basic,
+  shining,
+  cute,
+  smile,
+  thinking,
+  gloomy,
+  wrong,
+  jump,
+};
+
+/**
+ * @example 줄바꿈은 다음과 같이 사용해주세요.
+ * <Mascot text={`Hi buddy!\nWelcome to korea!`}/>
+ */
+function Mascot({ image, text }: MascotProps) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.message}>
+        <p className={styles.text}>{text}</p>
+      </div>
+
+      <img
+        className={styles.image}
+        src={IMAGE_MAP[image]}
+        alt="마스코트 이미지"
+      />
+    </div>
+  );
+}
+
+export default Mascot;
