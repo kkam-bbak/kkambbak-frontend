5b23d517d619e8a7dab73c68e3eabcf159ef27ad
presentKey <kim.hyeonjae4@gmail.com>
2025-11-19 17:10:38 +0900

    Feat: [REQ-0017] 결제 생성 api 적용

--- src/pages/PaymentPage/CheckoutPage/PaymentCheckoutPage.tsx
+++ src/pages/PaymentPage/CheckoutPage/PaymentCheckoutPage.tsx
@@ -4,9 +4,10 @@ import Mascot from '@/components/Mascot/Mascot';
 import styles from './PaymentCheckoutPage.module.css';
 import { useState } from 'react';
 import ContentSection from '@/components/layout/ContentSection/ContentSection';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { getSubscriptionPlans } from '@/apis/subscriptions';
 import kakao from '@/assets/kakaopay.png';
+import { createPayments } from '@/apis/payments';
 
 const PLAN = {
   PREMIUM: 'Premium',
@@ -21,7 +22,19 @@ function PaymentCheckoutPage() {
     queryFn: getSubscriptionPlans,
   });
 
+  const { mutate } = useMutation({
+    mutationFn: () => createPayments(selected),
+  });
+
   const { today, nextMonth } = getSubscriptionPeriod();
+
+  const handlePayClick = () => {
+    mutate(undefined, {
+      onSuccess: (data) => {
+        window.location.href = data.data.body.approvalURl;
+      },
+    });
+  };
 
   return (
     <div
@@ -72,7 +85,7 @@ function PaymentCheckoutPage() {
                     >
                       <option value="">Standard(₩ 0)</option>
                       {plans.map(({ id, name, price }) => (
-                        <option key={id} value={name}>
+                        <option key={id} value={id}>
                           {`${PLAN[name]}(Per Month ₩ ${price})`}
                         </option>
                       ))}
@@ -94,7 +107,7 @@ function PaymentCheckoutPage() {
               </div>
 
               {selected && (
-                <Button className={styles.pay} isFull>
+                <Button className={styles.pay} isFull onClick={handlePayClick}>
                   <img src={kakao} alt="카카오 페이" />
                 </Button>
               )}
