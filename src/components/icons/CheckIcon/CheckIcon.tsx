14169f2eb0721fd581eee70b78de889eaa6f2f25
presentKey <kim.hyeonjae4@gmail.com>
2025-11-21 04:49:10 +0900

    Feat: [UI] CheckIcon 추가

---
+++ src/components/icons/CheckIcon/CheckIcon.tsx
@@ -0,0 +1,19 @@
+function CheckIcon() {
+  return (
+    <svg
+      width="12"
+      height="10"
+      viewBox="0 0 12 10"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0.65625 4.15234C2.15625 5.46484 4.65625 7.65234 4.65625 7.65234L10.6562 0.652344"
+        stroke="black"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
+
+export default CheckIcon;
